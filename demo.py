5f000e09e55d85988d3bb83da909ad9281e59ceb
Haoran Feng <harrifeng@gmail.com>
2018-03-11 11:36:31 +0800

    go on ok

--- demo.py
+++ demo.py
@@ -1,12 +1,22 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 app.config.from_object('settings_dev')
 app.config.from_envvar('ABADMIN_CONFIG', silent=True)
 
+
+def return_404():
+    ret = {
+        'code': 0,
+        'message': '',
+    }
+    return jsonify(ret)
+
+
 @app.route('/')
 def hello():
+    return return_404()
     return 'hello world'
 
 
